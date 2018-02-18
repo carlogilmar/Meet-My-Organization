005e845ed35e71aaa1093166824b622c7f4385b1
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-17 23:38:46 -0600

    Adding addon for toast and put a simple input

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  actions:{
+    meetCommunity:function(username){
+      if(username){
+        this.get('controller').set('username', "");
+        this.toast.info('Finding your community: '+username);
+      }
+      else{
+        this.toast.warning('Community Empty');
+      }
+    }
+  }
+});
